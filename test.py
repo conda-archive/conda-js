e8629007907e3f809b33b7d5f77691223b7a6589
David Li <li.davidm96@gmail.com>
2014-07-21 11:03:08 -0500

    Add sample Python servers

---
+++ test.py
@@ -0,0 +1,32 @@
+import sys
+import flask
+
+method = 'rpc'
+if len(sys.argv) > 1:
+    if sys.argv[1] == '--rest':
+        method = 'rest'
+
+if method == 'rpc':
+    from agent import rpc as blueprint
+elif method == 'rest':
+    from agent import rest as blueprint
+
+
+if __name__ == '__main__':
+    app = flask.Flask(__name__, template_folder='./')
+    app.config['SECRET_KEY'] = 'secret'
+
+    @app.route('/<fname>')
+    def file(fname):
+        return open(fname).read()
+
+    @app.route('/')
+    def index():
+        return flask.render_template('test.html')
+
+    print("Using method", method)
+
+    blueprint.conda_js.url_prefix = '/api'
+    app.register_blueprint(blueprint.conda_js)
+
+    app.run(port=8000, debug=True)
