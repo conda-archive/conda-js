ef99b2f2e8fad53f8756a594d38d16636cd71225
David Li <li.davidm96@gmail.com>
2014-07-28 14:58:59 -0500

    Test conda-js against multiple endpoints

--- test.js
+++ test.js
@@ -9,302 +9,313 @@ else {
     conda = require('./conda');
 }
 
-function assertSuccess(result) {
-    assert.ok((typeof result.success !== "undefined") && result.success);
-}
+function test(conda) {
+    function assertSuccess(result) {
+        assert.ok((typeof result.success !== "undefined") && result.success);
+    }
 
-function assertError(result) {
-    assert.ok(typeof result.error !== "undefined");
-}
+    function assertError(result) {
+        assert.ok(typeof result.error !== "undefined");
+    }
 
-function assertType(type) {
-    return function(result) {
-        assert.equal(typeof result, type);
-    };
-}
+    function assertType(type) {
+        return function(result) {
+            assert.equal(typeof result, type);
+        };
+    }
 
-function assertInstance(klass) {
-    return function(result) {
-        assert.ok(result instanceof klass);
-    };
-}
+    function assertInstance(klass) {
+        return function(result) {
+            assert.ok(result instanceof klass);
+        };
+    }
 
-function assertKey(key) {
-    return function(result) {
-        assert.ok(typeof result[key] !== "undefined");
-    };
-}
+    function assertKey(key) {
+        return function(result) {
+            assert.ok(typeof result[key] !== "undefined");
+        };
+    }
 
-assertError = assertKey("error");
+    assertError = assertKey("error");
 
-function assertAll(asserts) {
-    return function(result) {
-        asserts.forEach(function(assert) {
-            assert(result);
-        });
-    };
-}
-
-describe('info', function() {
-    it('should return a dictionary', function(done) {
-        conda.info().then(assertType('object')).then(done);
-    });
-
-    it('should contain various keys', function(done) {
-        conda.info().then().then(assertAll([
-            assertKey('channels'),
-            assertKey('conda_version'),
-            assertKey('default_prefix'),
-            assertKey('envs'),
-            assertKey('envs_dirs'),
-            assertKey('is_foreign'),
-            assertKey('pkgs_dirs'),
-            assertKey('platform'),
-            assertKey('python_version'),
-            assertKey('rc_path'),
-            assertKey('root_prefix'),
-            assertKey('root_writable'),
-        ])).done(done);
-    });
-});
-
-describe('search', function() {
-    it('should return a dictionary', function(done) {
-        conda.search().then(assertType('object')).then(done);
-    });
-});
-
-describe('run', function() {
-    it('should return a dictionary', function(done) {
-        conda.run('nonexistent').then(assertType('object')).then(done);
-    });
-
-    it('should error for a nonexistent app', function(done) {
-        conda.run('nonexistent').then(assertError).then(done);
-    });
-
-    it('should error for a package that is not an app', function(done) {
-        conda.run('python').then(assertError).then(done);
-    });
-});
-
-describe('Config', function() {
-    var config = new conda.Config();
-    it("shouldn't accept both config and file", function(done) {
-        assert.throws(function() {
-            new conda.Config({ system: true, file: 'test' });
-        });
-        done();
-    });
-
-    describe("#get", function() {
-        it("should only accept certain keys", function(done) {
-            assert.throws(function() {
-                config.get('nonexistent_key').then(done);
+    function assertAll(asserts) {
+        return function(result) {
+            asserts.forEach(function(assert) {
+                assert(result);
             });
-            done();
-        });
+        };
+    }
 
-        it("should return a dictionary", function(done) {
-            config.get('channels').then(assertType('object')).done(done);
-        });
-    });
-
-    describe("#getAll", function() {
-        it("should return a dictionary", function(done) {
-            config.get('channels').then(assertType('object')).done(done);
-        });
-    });
-
-    describe("#set", function() {
-        it("should only accept certain keys", function(done) {
-            assert.throws(function() {
-                config.set('nonexistent_key', 'value').then(done);
-            });
-            done();
-        });
-
-        it("should succeed", function(done) {
-            config.set('use_pip', true).then(assertSuccess).then(function() {
-                config.set('use_pip', false).then(assertSuccess).done(done);
-            });
-        });
-    });
-});
-
-describe('Env', function() {
-    var envs;
-    before(function(done) {
-        conda.Env.getEnvs().then(function(result) {
-            envs = result;
-            done();
-        });
-    });
-
-    describe('.create', function() {
-        it('should support progressbars', function(done) {
-            var first = true;
-            conda.Env.create({ name: 'testing', packages: ['_license'], progress: true })
-                .progress(function(progress) {
-                    if (first) {
-                        done();
-                        first = false;
-                    }
-                }).done(function() {
-                    // happens when env was created beforehand
-                    if (first) {
-                        done();
-                    }
-                });
-        });
-    });
-
-    describe('.getEnvs', function() {
-        it('should return a list of Envs', function() {
-            assertInstance(conda.Env)(envs[0]);
-        });
-    });
-
-    describe('#install', function() {
+    describe('info', function() {
         it('should return a dictionary', function(done) {
-            envs[0].install({ packages: ['python'] })
-                .then(assertSuccess).done(done);
+            conda.info().then(assertType('object')).then(done);
+        });
+
+        it('should contain various keys', function(done) {
+            conda.info().then().then(assertAll([
+                assertKey('channels'),
+                assertKey('conda_version'),
+                assertKey('default_prefix'),
+                assertKey('envs'),
+                assertKey('envs_dirs'),
+                assertKey('is_foreign'),
+                assertKey('pkgs_dirs'),
+                assertKey('platform'),
+                assertKey('python_version'),
+                assertKey('rc_path'),
+                assertKey('root_prefix'),
+                assertKey('root_writable'),
+            ])).done(done);
         });
     });
 
-    describe('#linked', function() {
-        it('should return a list of Packages', function(done) {
-            envs[0].linked().then(function(result) {
-                assert.ok(Array.isArray(result));
-                assertInstance(conda.Package)(result[0]);
-            }).done(done);
-        });
-
-        it('should return a list of strings with simple=true', function(done) {
-            envs[0].linked({ simple: true }).then(function(result) {
-                assert.ok(Array.isArray(result));
-                assertType('string')(result[0]);
-            }).done(done);
+    describe('search', function() {
+        it('should return a dictionary', function(done) {
+            conda.search().then(assertType('object')).then(done);
         });
     });
 
-    describe('#revisions', function() {
-        it('should return a list of objects', function(done) {
-            envs[0].revisions().then(function(result) {
-                assert.ok(Array.isArray(result));
-                assertType('object')(result[0]);
-            }).done(done);
+    describe('run', function() {
+        it('should return a dictionary', function(done) {
+            conda.run('nonexistent').then(assertType('object')).then(done);
+        });
+
+        it('should error for a nonexistent app', function(done) {
+            conda.run('nonexistent').then(assertError).then(done);
+        });
+
+        it('should error for a package that is not an app', function(done) {
+            conda.run('python').then(assertError).then(done);
         });
     });
 
-    // TODO this is extremely slow, times out
-    // describe('#removeEnv', function() {
-    //     it('should return a dictionary', function(done) {
-    //         envs.forEach(function(env) {
-    //             if (env.name === 'testing') {
-    //                 env.removeEnv().then(assertType('object')).done(done);
-    //             }
-    //         });
-    //     });
-    // });
-});
-
-
-describe('Package', function() {
-    describe('.parseVersion', function() {
-        it('should parse any number of parts', function() {
-            assert.deepEqual(conda.Package.parseVersion("1.2.3.4.5").parts, [1, 2, 3, 4, 5]);
+    describe('Config', function() {
+        var config = new conda.Config();
+        it("shouldn't accept both config and file", function(done) {
+            assert.throws(function() {
+                new conda.Config({ system: true, file: 'test' });
+            });
+            done();
         });
 
-        it('should parse RC parts', function() {
-            assert.deepEqual(conda.Package.parseVersion("1.2.3rc1").parts, [1, 2, 3]);
-            assert.deepEqual(conda.Package.parseVersion("1.2.3rc1").rc, 1);
-            assert.deepEqual(conda.Package.parseVersion("1.2.3").rc, null);
-        });
-    });
+        describe("#get", function() {
+            it("should only accept certain keys", function(done) {
+                assert.throws(function() {
+                    config.get('nonexistent_key').then(done);
+                });
+                done();
+            });
 
-    describe('.isGreater', function() {
-        it('should compare build numbers', function() {
-            assert.ok(conda.Package.isGreater({
-                version: "1.2.3",
-                build_number: 2
-            }, {
-                version: "1.2.3",
-                build_number: 3
-            }));
+            it("should return a dictionary", function(done) {
+                config.get('channels').then(assertType('object')).done(done);
+            });
         });
 
-        it('should compare versions', function() {
-            assert.ok(conda.Package.isGreater({
-                version: "1.2.3",
-                build_number: 3
-            }, {
-                version: "1.2.6",
-                build_number: 3
-            }));
-            assert.ok(conda.Package.isGreater({
-                version: "1.2.3",
-                build_number: 3
-            }, {
-                version: "1.3.0",
-                build_number: 3
-            }));
-            assert.ok(conda.Package.isGreater({
-                version: "1.2.3",
-                build_number: 3
-            }, {
-                version: "2.0.0",
-                build_number: 3
-            }));
-            assert.ok(conda.Package.isGreater({
-                version: "1.12.30",
-                build_number: 3
-            }, {
-                version: "2.10.0",
-                build_number: 3
-            }));
-            assert.ok(conda.Package.isGreater({
-                version: "19.12.30",
-                build_number: 3
-            }, {
-                version: "21.10.15",
-                build_number: 3
-            }));
-            assert.ok(conda.Package.isGreater({
-                version: "19.12.3",
-                build_number: 3
-            }, {
-                version: "19.12.15",
-                build_number: 3
-            }));
+        describe("#getAll", function() {
+            it("should return a dictionary", function(done) {
+                config.get('channels').then(assertType('object')).done(done);
+            });
         });
 
-        it('should compare versions of unequal length', function() {
-            assert.ok(conda.Package.isGreater({
-                version: "1.2",
-                build_number: 3
-            }, {
-                version: "1.2.3",
-                build_number: 3
-            }));
-        });
+        describe("#set", function() {
+            it("should only accept certain keys", function(done) {
+                assert.throws(function() {
+                    config.set('nonexistent_key', 'value').then(done);
+                });
+                done();
+            });
 
-        it('should compare release candidate numbers', function() {
-            assert.ok(conda.Package.isGreater({
-                version: "1.2.3rc1",
-                build_number: 3
-            }, {
-                version: "1.2.3",
-                build_number: 3
-            }));
-
-            assert.ok(conda.Package.isGreater({
-                version: "1.2.3rc1",
-                build_number: 3
-            }, {
-                version: "1.2.3rc2",
-                build_number: 3
-            }));
+            it("should succeed", function(done) {
+                config.set('use_pip', true).then(assertSuccess).then(function() {
+                    config.set('use_pip', false).then(assertSuccess).done(done);
+                });
+            });
         });
     });
-});
+
+    describe('Env', function() {
+        var envs;
+        before(function(done) {
+            conda.Env.getEnvs().then(function(result) {
+                envs = result;
+                done();
+            });
+        });
+
+        describe('.create', function() {
+            it('should support progressbars', function(done) {
+                var first = true;
+                conda.Env.create({ name: 'testing', packages: ['_license'], progress: true })
+                    .progress(function(progress) {
+                        if (first) {
+                            done();
+                            first = false;
+                        }
+                    }).done(function() {
+                        // happens when env was created beforehand
+                        if (first) {
+                            done();
+                        }
+                    });
+            });
+        });
+
+        describe('.getEnvs', function() {
+            it('should return a list of Envs', function() {
+                assertInstance(conda.Env)(envs[0]);
+            });
+        });
+
+        describe('#install', function() {
+            it('should return a dictionary', function(done) {
+                envs[0].install({ packages: ['python'] })
+                    .then(assertSuccess).done(done);
+            });
+        });
+
+        describe('#linked', function() {
+            it('should return a list of Packages', function(done) {
+                envs[0].linked().then(function(result) {
+                    assert.ok(Array.isArray(result));
+                    assertInstance(conda.Package)(result[0]);
+                }).done(done);
+            });
+
+            it('should return a list of strings with simple=true', function(done) {
+                envs[0].linked({ simple: true }).then(function(result) {
+                    assert.ok(Array.isArray(result));
+                    assertType('string')(result[0]);
+                }).done(done);
+            });
+        });
+
+        describe('#revisions', function() {
+            it('should return a list of objects', function(done) {
+                envs[0].revisions().then(function(result) {
+                    assert.ok(Array.isArray(result));
+                    assertType('object')(result[0]);
+                }).done(done);
+            });
+        });
+
+        // TODO this is extremely slow, times out
+        // describe('#removeEnv', function() {
+        //     it('should return a dictionary', function(done) {
+        //         envs.forEach(function(env) {
+        //             if (env.name === 'testing') {
+        //                 env.removeEnv().then(assertType('object')).done(done);
+        //             }
+        //         });
+        //     });
+        // });
+    });
+
+
+    describe('Package', function() {
+        describe('.parseVersion', function() {
+            it('should parse any number of parts', function() {
+                assert.deepEqual(conda.Package.parseVersion("1.2.3.4.5").parts, [1, 2, 3, 4, 5]);
+            });
+
+            it('should parse RC parts', function() {
+                assert.deepEqual(conda.Package.parseVersion("1.2.3rc1").parts, [1, 2, 3]);
+                assert.deepEqual(conda.Package.parseVersion("1.2.3rc1").rc, 1);
+                assert.deepEqual(conda.Package.parseVersion("1.2.3").rc, null);
+            });
+        });
+
+        describe('.isGreater', function() {
+            it('should compare build numbers', function() {
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2.3",
+                    build_number: 2
+                }, {
+                    version: "1.2.3",
+                    build_number: 3
+                }));
+            });
+
+            it('should compare versions', function() {
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2.3",
+                    build_number: 3
+                }, {
+                    version: "1.2.6",
+                    build_number: 3
+                }));
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2.3",
+                    build_number: 3
+                }, {
+                    version: "1.3.0",
+                    build_number: 3
+                }));
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2.3",
+                    build_number: 3
+                }, {
+                    version: "2.0.0",
+                    build_number: 3
+                }));
+                assert.ok(conda.Package.isGreater({
+                    version: "1.12.30",
+                    build_number: 3
+                }, {
+                    version: "2.10.0",
+                    build_number: 3
+                }));
+                assert.ok(conda.Package.isGreater({
+                    version: "19.12.30",
+                    build_number: 3
+                }, {
+                    version: "21.10.15",
+                    build_number: 3
+                }));
+                assert.ok(conda.Package.isGreater({
+                    version: "19.12.3",
+                    build_number: 3
+                }, {
+                    version: "19.12.15",
+                    build_number: 3
+                }));
+            });
+
+            it('should compare versions of unequal length', function() {
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2",
+                    build_number: 3
+                }, {
+                    version: "1.2.3",
+                    build_number: 3
+                }));
+            });
+
+            it('should compare release candidate numbers', function() {
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2.3rc1",
+                    build_number: 3
+                }, {
+                    version: "1.2.3",
+                    build_number: 3
+                }));
+
+                assert.ok(conda.Package.isGreater({
+                    version: "1.2.3rc1",
+                    build_number: 3
+                }, {
+                    version: "1.2.3rc2",
+                    build_number: 3
+                }));
+            });
+        });
+    });
+}
+
+test(conda);
+
+if (typeof conda.newContext !== "undefined") {
+    var context = conda.newContext();
+    context.API_METHOD = 'REST';
+    context.API_ROOT = 'http://localhost:8001/api';
+    test(context);
+}
--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import sys
 import flask
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rpc', action='store_true', default=False)
+parser.add_argument('--rest', action='store_true', default=False)
+parser.add_argument('--no-websockets', action='store_true', default=False)
+parser.add_argument('--cross-origin', action='store_true', default=False)
+parser.add_argument('--port', type=int, default=8000)
+parser.add_argument('--root', type=str, default='/api')
+
+args = parser.parse_args()
 
 method = 'rpc'
-if '--rest' in sys.argv:
+if args.rest:
     method = 'rest'
 
 if method == 'rpc':
@@ -37,16 +48,27 @@ if __name__ == '__main__':
 
     print("Using method", method)
 
-    blueprint.conda_js.url_prefix = '/api'
+    blueprint.conda_js.url_prefix = args.root
+
+    if args.cross_origin:
+        print("Allowing cross origin responses")
+
+        @blueprint.conda_js.after_request
+        def add_cross_origin(response):
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+            return response
+
     app.register_blueprint(blueprint.conda_js)
 
     if '--no-progress' in sys.argv:
-        app.run(port=8000, debug=True)
+        app.run(port=args.port, debug=True)
     else:
         print("Using websockets")
 
         import tornado.ioloop
         from agent.websocket import wrap
-        wsgi_app, application = wrap(app, '/api_ws', debug=False)
-        application.listen(8000)
+        wsgi_app, application = wrap(app, args.root + '_ws', debug=False)
+        application.listen(args.port)
         tornado.ioloop.IOLoop.instance().start()
